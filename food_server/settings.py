4fbeaaf72c0afbee9251bb30ac063f9a07b6a7b1
kosa0914 <kosa0914@naver.com>
2022-02-03 19:54:10 +0900

    test

--- food_server/settings.py
+++ food_server/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com",
-                 "foodsearch.shop",
+                 ".foodsearch.shop",
                  '127.0.0.1']
 
 
